0a252f4029c8d9f184341689fc9dd485da91ed21
Justin Kim <jinsukmik@gmail.com>
2017-07-23 11:22:08 -0500

    Add files via upload

---
+++ key.js
@@ -0,0 +1,13 @@
+export.twitterKeys = {
+  consumer_key: 'ElluTL2zGDpHXj37FH3JEVOwe',
+  consumer_secret: '0UTWfADFbu1jsxz6x9BHHR8J661Szkc0z6SXDnSAwKunKzQpXf',
+  access_token_key: '887098930139418624-1XJ8XODop113RbFSC5F3ChJneABrfXX',
+  access_token_secret: '7pauzYfJqmdZrdZetmZwBlMlkBBECGs1I2M68Okbv8Yyv',
+}
+
+var consumer_key = twitterKeys.consumer_key;
+var consumer_secret = twitterKeys.consumer_secret;
+var tokenKey = twitterKeys.access_token_key;
+var tokenSecret = twitterKeys.access_token_secret;
+
+var consumer_key = twitterKeys.con
